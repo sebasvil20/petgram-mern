c75b23410b4dc757c22d13ac2a2fb026299e184e
Sebastian Villegas <sebasvil20@gmail.com>
2021-04-01 11:19:32 -0500

    Adding PWA support, manifest and serviceWorker

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   performance: {
@@ -14,6 +17,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram',
+      description: 'Con petgram puedes encontrar fotos de animales domésticos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching:[
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com | images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options:{
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-hinval.vercel.app'),
+          handler: 'NetworkFirst',
+          options:{
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
